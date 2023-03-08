591af54a3541cdcb9a7ef43f310c18e7871ebef6
wugi <17031278@itcelaya.edu.mx>
2023-03-08 01:01:29 -0600

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+export default function()
+{
+   return(
+      <main className="main-div">
+         <h1 className="title">Fun facts about React</h1>
+
+         <ul className="list">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K star on GitHub</li>
+            <li>Is mantained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+         </ul>
+      </main>
+   )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import reactLogo from '../assets/react.svg'
+
+export default function()
+{
+   return(
+      <nav>
+         <img className="nav-logo" src={reactLogo} />
+
+         <h3 className="nav-logo-text">ReactFacts</h3>
+         <h4 className="nav-title">React Course - Project 1</h4>
+      </nav>
+   )
+}
